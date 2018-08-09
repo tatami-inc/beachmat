11a692492f969c760634f55610751eb338ca7a72
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-09 19:10:15 +0100

    Tweaks to internal test methods to be robust to zero-dimensions.

--- inst/testpkg/R/utils.R
+++ inst/testpkg/R/utils.R
@@ -1,11 +1,11 @@
 spawn_row_ordering <- function(NROW) {
-    rranges <- list(forward=seq_len(NROW), random=sample(NROW), subset=sample(NROW, NROW/2L))
+    rranges <- list(forward=seq_len(NROW), random=sample(NROW, NROW), subset=sample(NROW, NROW/2L))
     rranges$reverse <- rev(rranges$forward)
     rranges
 }
 
 spawn_col_ordering <- function(NCOL) {
-    cranges <- list(forward=seq_len(NCOL), random=sample(NCOL), subset=sample(NCOL, NCOL/2L))
+    cranges <- list(forward=seq_len(NCOL), random=sample(NCOL, NCOL), subset=sample(NCOL, NCOL/2L))
     cranges$reverse <- rev(cranges$forward)
     cranges
 }
