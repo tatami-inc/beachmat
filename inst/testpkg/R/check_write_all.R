2425462fdb19593239de8ee4e246631ccdcde0a7
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-09 10:36:19 +0100

    Added R/C++ functions to test writing entire rows/columns.

---
+++ inst/testpkg/R/check_write_all.R
@@ -0,0 +1,70 @@
+#' @export
+#' @importFrom testthat expect_identical
+check_write_all <- function(FUN, ..., mode, out.class) {
+    check_write_all_row(FUN(...), mode, out.class)
+    check_write_all_col(FUN(...), mode, out.class)
+    check_write_all_single(FUN(...), mode, out.class)
+}
+
+#' @importFrom testthat expect_identical
+check_write_all_row <- function(test.mat, mode, out.class, FUN="set_row_all") {
+    ref <- as.matrix(test.mat)
+    dimnames(ref) <- NULL
+
+    rranges <- spawn_row_ordering(nrow(test.mat))
+    for (o in rranges) {
+        out <- .Call(paste0(FUN, "_", mode), test.mat, o, PACKAGE="beachtest")
+        expect_identical(as.character(class(out[[1]])), out.class)
+
+        REF <- ref
+        REF[] <- get(mode)(1)
+        REF[seq_along(o),] <- ref[o,]
+        expect_equal(REF, as.matrix(out[[1]]))
+
+        expect_equal(REF[o,,drop=FALSE], out[[2]])
+    }
+    return(invisible(NULL))
+}
+
+#' @importFrom testthat expect_identical
+check_write_all_col <- function(test.mat, mode, out.class, FUN="set_col_all") {
+    ref <- as.matrix(test.mat)
+    dimnames(ref) <- NULL
+
+    cranges <- spawn_col_ordering(ncol(test.mat))
+    for (o in cranges) {
+        out <- .Call(paste0(FUN, "_", mode), test.mat, o, PACKAGE="beachtest")
+        expect_identical(as.character(class(out[[1]])), out.class)
+
+        REF <- ref
+        REF[] <- get(mode)(1)
+        REF[,seq_along(o)] <- ref[,o]
+        expect_equal(REF, as.matrix(out[[1]]));
+
+        expect_equal(REF[,o,drop=FALSE], out[[2]])
+    }
+    return(invisible(NULL))
+}
+
+#' @importFrom testthat expect_identical
+check_write_all_single <- function(test.mat, mode, out.class, FUN="set_single_all") {
+    ref <- as.matrix(test.mat)
+    dimnames(ref) <- NULL
+
+    rranges <- spawn_row_ordering(nrow(test.mat))
+    cranges <- spawn_col_ordering(ncol(test.mat))
+    for (ro in rranges) {
+        for (co in cranges) { 
+            out <- .Call(paste0(FUN, "_", mode), test.mat, ro, co, PACKAGE="beachtest")
+            expect_identical(as.character(class(out[[1]])), out.class)
+
+            REF <- ref
+            REF[] <- get(mode)(1)
+            REF[seq_along(ro),seq_along(co)] <- ref[ro, co]
+            expect_equal(REF, as.matrix(out[[1]]))
+
+            expect_equal(REF[ro,co,drop=FALSE], out[[2]])
+        }
+    }
+    return(invisible(NULL))
+}
