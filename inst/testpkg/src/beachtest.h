2338a1d6719ac1a8735010e5dd277da7660fe249
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-10 10:07:25 +0100

    Generalized class checking test code for input matrices.

--- inst/testpkg/src/beachtest.h
+++ inst/testpkg/src/beachtest.h
@@ -7,4 +7,6 @@
 #include "beachmat/logical_matrix.h"
 #include "beachmat/character_matrix.h"
 
+Rcpp::StringVector translate_class(beachmat::matrix_type);
+
 #endif
--- inst/testpkg/src/set_class.cpp
+++ inst/testpkg/src/set_class.cpp
@@ -4,20 +4,7 @@ extern "C" {
 
 SEXP set_class_by_sexp(SEXP incoming, SEXP simplify, SEXP preserve_zero) {
     beachmat::output_param op(incoming, Rf_asLogical(simplify), Rf_asLogical(preserve_zero));
-
-    Rcpp::StringVector out(1);
-    switch (op.get_mode()) {
-        case beachmat::SIMPLE:
-            out[0]="simple";
-            return out;
-        case beachmat::SPARSE:
-            out[0]="sparse";
-            return out;
-        case beachmat::HDF5:
-            out[0]="HDF5";
-            return out;
-    }
-    throw std::runtime_error("invalid output type");
+    return translate_class(op.get_mode());
 }
 
 }
