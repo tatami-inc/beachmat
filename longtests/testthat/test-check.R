788a888eea1c6db33e7d136e4ace02e92f064378
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-17 11:59:57 +0100

    Added extension test check to longtests.

--- longtests/testthat/test-check.R
+++ longtests/testthat/test-check.R
@@ -2,3 +2,8 @@ test_that("internal test package CHECKs correctly", {
     out <- devtools::check(system.file("testpkg", package="beachmat"), document=FALSE)
     expect_identical(out$error, character(0))
 })
+
+test_that("extension test package CHECKs correctly", {
+    out <- devtools::check(system.file("extensions", package="beachmat"), document=FALSE)
+    expect_identical(out$error, character(0))
+})
