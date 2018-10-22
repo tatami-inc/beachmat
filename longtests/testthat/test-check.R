aac161dd818c03353bd1736d844aea2a6adcbd20
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-22 10:32:26 +0100

    Avoid longtest failure upon CHECK warnings.

--- longtests/testthat/test-check.R
+++ longtests/testthat/test-check.R
@@ -1,9 +1,7 @@
 test_that("internal test package CHECKs correctly", {
-    out <- devtools::check(system.file("testpkg", package="beachmat"), document=FALSE)
-    expect_identical(out$error, character(0))
+    expect_error(devtools::check(system.file("testpkg", package="beachmat"), document=FALSE, error_on="error"), NA)
 })
 
 test_that("extension test package CHECKs correctly", {
-    out <- devtools::check(system.file("extensions", package="beachmat"), document=FALSE)
-    expect_identical(out$error, character(0))
+    expect_error(devtools::check(system.file("extensions", package="beachmat"), document=FALSE, error_on="error"), NA)
 })
