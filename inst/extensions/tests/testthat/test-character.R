1351126189bf0d479a4c2598ed17b2f06467b5fa
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-03-08 10:00:28 -0800

    Added output tests to extension package.

--- inst/extensions/tests/testthat/test-character.R
+++ inst/extensions/tests/testthat/test-character.R
@@ -22,11 +22,34 @@ test_that("Aaron's Matrix can be read by beachmat", {
     check_read_multi(generator, nr=5, nc=30, mode="character")
     check_read_multi(generator, nr=30, nc=5, mode="character")
 
-    check_read_type(generator, mode="character")
     check_read_class(generator(), mode="character", "AaronMatrix")
 
     check_read_errors(generator, mode="character")
     check_read_all(generator, nr=0, nc=0, mode="character")
     check_read_all(generator, nr=10, nc=0, mode="character")
     check_read_all(generator, nr=0, nc=10, mode="character")
+})
+
+test_that("Aaron's Matrix can be written by beachmat", {
+    check_write_all(generator, mode="character")
+    check_write_all(generator, nr=5, nc=30, mode="character")
+    check_write_all(generator, nr=30, nc=5, mode="character")
+
+    check_write_slice(generator, mode="character")
+    check_write_slice(generator, nr=5, nc=30, mode="character")
+    check_write_slice(generator, nr=30, nc=5, mode="character")
+
+    check_write_varslice(generator, mode="character")
+    check_write_varslice(generator, nr=5, nc=30, mode="character")
+    check_write_varslice(generator, nr=30, nc=5, mode="character")
+
+    check_write_indexed(generator, mode="character")
+    check_write_indexed(generator, nr=5, nc=30, mode="character")
+    check_write_indexed(generator, nr=30, nc=5, mode="character")
+
+    check_write_errors(generator, mode="character")
+
+    check_write_all(generator, nr=0, nc=0, mode="character")
+    check_write_all(generator, nr=10, nc=0, mode="character")
+    check_write_all(generator, nr=0, nc=10, mode="character")
 })
--- inst/extensions/tests/testthat/test-integer.R
+++ inst/extensions/tests/testthat/test-integer.R
@@ -30,3 +30,28 @@ test_that("Aaron's Matrix can be read by beachmat", {
     check_read_all(generator, nr=10, nc=0, mode="integer")
     check_read_all(generator, nr=0, nc=10, mode="integer")
 })
+
+test_that("Aaron's Matrix can be written by beachmat", {
+    check_write_all(generator, mode="integer")
+    check_write_all(generator, nr=5, nc=30, mode="integer")
+    check_write_all(generator, nr=30, nc=5, mode="integer")
+
+    check_write_slice(generator, mode="integer")
+    check_write_slice(generator, nr=5, nc=30, mode="integer")
+    check_write_slice(generator, nr=30, nc=5, mode="integer")
+
+    check_write_varslice(generator, mode="integer")
+    check_write_varslice(generator, nr=5, nc=30, mode="integer")
+    check_write_varslice(generator, nr=30, nc=5, mode="integer")
+
+    check_write_indexed(generator, mode="integer")
+    check_write_indexed(generator, nr=5, nc=30, mode="integer")
+    check_write_indexed(generator, nr=30, nc=5, mode="integer")
+
+    check_write_type(generator, mode="integer")
+    check_write_errors(generator, mode="integer")
+
+    check_write_all(generator, nr=0, nc=0, mode="integer")
+    check_write_all(generator, nr=10, nc=0, mode="integer")
+    check_write_all(generator, nr=0, nc=10, mode="integer")
+})
