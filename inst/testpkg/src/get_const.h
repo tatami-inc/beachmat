b265535ddc9c507b9bc2d66a3f2d776cff5aff06
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-03-16 09:26:21 -0700

    Update specialized getter tests to switch based on raw type.

--- inst/testpkg/src/get_const.h
+++ inst/testpkg/src/get_const.h
@@ -7,12 +7,20 @@ O get_const_all (M ptr, Rcpp::IntegerVector ordering) {
     const size_t nrows=ptr->get_nrow();
     O output(nrows, ordering.size());
 
+    const bool use_const=(ptr->col_raw_type()=="dense");
     auto raws=ptr->set_up_raw();
+    T alternative(nrows);
+    auto it=alternative.begin();
     size_t c=0;
 
     for (auto o : ordering) {
-        ptr->get_col_raw(o-1, raws);
-        auto it=raws.get_values_start();
+        if (use_const) {
+            ptr->get_col_raw(o-1, raws);
+            it=raws.get_values_start();
+        } else {
+            ptr->get_col(o-1, it);
+        }
+
         auto outcol=output.column(c);
         std::copy(it, it + nrows, outcol.begin());
         ++c;
@@ -27,14 +35,22 @@ O get_const_slice (M ptr, Rcpp::IntegerVector ordering, Rcpp::IntegerVector rows
     }
     const int rstart=rows[0]-1, rend=rows[1];
     const int nrows=rend-rstart;    
-
-    auto raws=ptr->set_up_raw();
     O output(nrows, ordering.size());
+
+    const bool use_const=(ptr->col_raw_type()=="dense");
+    auto raws=ptr->set_up_raw();
+    T alternative(nrows);
+    auto it=alternative.begin();
     size_t c=0;
 
     for (auto o : ordering) {
-        ptr->get_col_raw(o-1, raws, rstart, rend);
-        auto it=raws.get_values_start();
+        if (use_const) {
+            ptr->get_col_raw(o-1, raws, rstart, rend);
+            it=raws.get_values_start();
+        } else {
+            ptr->get_col(o-1, it, rstart, rend);
+        }
+
         auto curcol=output.column(c);
         std::copy(it, it+nrows, curcol.begin());
         ++c;
@@ -51,16 +67,24 @@ Rcpp::List get_const_varslice (M ptr, Rcpp::IntegerVector ordering, Rcpp::Intege
     if (rows.nrow()!=ordering.size()) {
         throw std::runtime_error("'nrow(rows)' should be equal to 'length(ordering)'");
     }
-
-    auto raws=ptr->set_up_raw();
     Rcpp::List output(ordering.size());
+
+    const bool use_const=(ptr->col_raw_type()=="dense");
+    auto raws=ptr->set_up_raw();
+    T alternative(ptr->get_nrow());
+    auto it=alternative.begin();
     size_t c=0;
 
     for (auto o : ordering) {
         auto cur_bounds=rows.row(c);
         int left=cur_bounds[0]-1, right=cur_bounds[1];
-        ptr->get_col_raw(o-1, raws, left, right);
-        auto it=raws.get_values_start();
+
+        if (use_const) {
+            ptr->get_col_raw(o-1, raws, left, right);
+            it=raws.get_values_start();
+        } else {
+            ptr->get_col(o-1, it, left, right);
+        }
 
         T out(right-left);
         std::copy(it, it+right-left, out.begin());
--- inst/testpkg/src/get_indexed.h
+++ inst/testpkg/src/get_indexed.h
@@ -1,20 +1,38 @@
 #ifndef BEACHTEST_GET_INDEXED_H
 #define BEACHTEST_GET_INDEXED_H
 #include "beachtest.h"
+#include <algorithm>
+
+Rcpp::IntegerVector spawn_indices(size_t n) {
+    Rcpp::IntegerVector out(n);
+    std::iota(out.begin(), out.end(), 0);
+    return out;
+}
 
 template <class T, class O, class M>  // M is automatically deduced.
 O get_indexed_all (M ptr, Rcpp::IntegerVector ordering) {
     const size_t& nrows=ptr->get_nrow();
     O output(nrows, ordering.size());
 
+    const bool use_sparse=(ptr->col_raw_type()=="sparse");
     auto raws=ptr->set_up_raw();
+    T alternative(nrows);
     size_t c=0;
 
+    auto vals=alternative.begin();
+    size_t N=nrows;
+    Rcpp::IntegerVector counter(spawn_indices(nrows));
+    auto idx=counter.begin();
+
     for (auto o : ordering) {
-        ptr->get_col_raw(o-1, raws);
-        size_t N=raws.get_n();
-        auto idx=raws.get_structure_start();
-        auto vals=raws.get_values_start();
+        if (use_sparse) {
+            ptr->get_col_raw(o-1, raws);
+            N=raws.get_n();
+            idx=raws.get_structure_start();
+            vals=raws.get_values_start();
+        } else {
+            ptr->get_col(o-1, vals);
+        }
 
         auto outcol=output.column(c);
         for (size_t i=0; i<N; ++i) {
@@ -31,17 +49,31 @@ O get_indexed_slice (M ptr, Rcpp::IntegerVector ordering, Rcpp::IntegerVector ro
         throw std::runtime_error("'rows' should be an integer vector of length 2"); 
     }
     const int rstart=rows[0]-1, rend=rows[1];
-    const int nrows=rend-rstart;    
+    const int out_nrows=rend-rstart;
+    O output(out_nrows, ordering.size());
 
+    const bool use_sparse=(ptr->col_raw_type()=="sparse");
     auto raws=ptr->set_up_raw();
-    O output(nrows, ordering.size());
+    T alternative(out_nrows);
     size_t c=0;
 
+    auto vals=alternative.begin();
+    Rcpp::IntegerVector counter(spawn_indices(ptr->get_nrow()));
+
     for (auto o : ordering) {
-        ptr->get_col_raw(o-1, raws, rstart, rend);
-        size_t N=raws.get_n();
-        auto idx=raws.get_structure_start();
-        auto vals=raws.get_values_start();
+        size_t N;
+        Rcpp::IntegerVector::iterator idx;
+
+        if (use_sparse) { 
+            ptr->get_col_raw(o-1, raws, rstart, rend);
+            N=raws.get_n();
+            idx=raws.get_structure_start();
+            vals=raws.get_values_start();
+        } else {
+            ptr->get_col(o-1, vals, rstart, rend);
+            idx=counter.begin()+rstart;
+            N=rend-rstart;
+        }
 
         auto curcol=output.column(c);
         for (size_t i=0; i<N; ++i) {
@@ -61,19 +93,33 @@ Rcpp::List get_indexed_varslice (M ptr, Rcpp::IntegerVector ordering, Rcpp::Inte
     if (rows.nrow()!=ordering.size()) {
         throw std::runtime_error("'nrow(rows)' should be equal to 'length(ordering)'");
     }
-
     Rcpp::List output(ordering.size());
+    const size_t nrows=ptr->get_nrow();
+
+    const bool use_sparse=(ptr->col_raw_type()=="sparse");
     auto raws=ptr->set_up_raw();
+    T alternative(nrows);
     size_t c=0;
+
+    auto vals=alternative.begin();
+    Rcpp::IntegerVector counter(spawn_indices(nrows));
 
     for (auto o : ordering) {
         auto cur_bounds=rows.row(c);
         int left=cur_bounds[0]-1, right=cur_bounds[1];
+        size_t N;
+        Rcpp::IntegerVector::iterator idx;
 
-        ptr->get_col_raw(o-1, raws, left, right);
-        size_t N=raws.get_n();
-        auto idx=raws.get_structure_start();
-        auto vals=raws.get_values_start();
+        if (use_sparse) {
+            ptr->get_col_raw(o-1, raws, left, right);
+            N=raws.get_n();
+            idx=raws.get_structure_start();
+            vals=raws.get_values_start();
+        } else {
+            ptr->get_col(o-1, vals, left, right);
+            idx=counter.begin()+left;
+            N=right-left;
+        }
 
         T out(right-left);
         for (size_t i=0; i<N; ++i) {
