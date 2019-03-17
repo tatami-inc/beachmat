cd34e95a321b53ed61b9c83537239b09fbb920af
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-03-17 14:57:22 -0700

    Made raw_structure constructor do some work. Modified vector getters to return a reference.

--- inst/include/beachmat/utils/raw_structure.h
+++ inst/include/beachmat/utils/raw_structure.h
@@ -9,7 +9,9 @@ namespace beachmat {
 template<class V>
 class raw_structure {
 public:
-    raw_structure()=default;
+    raw_structure(size_t nv=0, size_t ns=0) : values(nv), structure(ns), 
+        values_start(values.vec.begin()), structure_start(structure.vec.begin()) {}
+
     ~raw_structure()=default;
     
     raw_structure(const raw_structure& in) : n(in.n), 
@@ -38,17 +40,19 @@ public:
     Rcpp::IntegerVector::iterator& get_structure_start() { return structure_start; }
     typename V::iterator& get_values_start() { return values_start; }
 
-    // Not for external use.
-    Rcpp::IntegerVector get_structure() const { return structure.vec; }
-    V get_values() const { return values.vec; }
+    // Not for external use. If the vector ends up being reassigned 
+    // (unlikely; you should prefer to use the constructor for choosing the size),
+    // it is imporant that the iterators are re-initialized!
+    Rcpp::IntegerVector& get_structure() { return structure.vec; }
+    V& get_values() { return values.vec; }
 private:
-    size_t n=0;
-    Rcpp::IntegerVector::iterator structure_start;
-    typename V::iterator values_start;
-
     bool use_own=true;
     copyable_holder<Rcpp::IntegerVector> structure;
     copyable_holder<V> values;
+
+    size_t n=0;
+    Rcpp::IntegerVector::iterator structure_start;
+    typename V::iterator values_start;
 
     void initialize_iterators(const raw_structure& in) {
         if (use_own) {
@@ -59,7 +63,6 @@ private:
             values_start=in.values_start;
         }
     }
-
 };
 
 }
