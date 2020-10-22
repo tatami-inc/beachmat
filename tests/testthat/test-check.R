e3a9842d5029a3d76d9236f1597587112c12189d
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-10-22 00:51:22 -0700

    Don't check multiple architectures for the test package.

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -1,3 +1,5 @@
 test_that("internal test package CHECKs correctly", {
-    expect_error(devtools::check(system.file("newtests", package="beachmat"), document=FALSE, error_on="error"), NA)
+    expect_error(devtools::check(system.file("newtests", package="beachmat"), 
+        args=c("--no-multiarch"),
+        document=FALSE, error_on="error"), NA)
 })
