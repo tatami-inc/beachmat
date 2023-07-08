29a0991e59ef5cdec5716a768010a87c0f834a34
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-07-08 14:43:19 -0700

    Gave up with the internal package checks on windows.

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -1,4 +1,5 @@
 test_that("internal test package CHECKs correctly", {
+    skip_on_os("windows")
     expect_error(rcmdcheck::rcmdcheck(system.file("newtests", package="beachmat"), 
         args=c("--no-multiarch"), error_on="error"), NA)
 })
